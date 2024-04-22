b21a1d7748ee04e1f94c7e084e24b444953613bf
Anderson Beauvais <50684207+Andy7911@users.noreply.github.com>
2024-04-22 14:07:26 -0400

    add card

--- app/model/anime.py
+++ app/model/anime.py
@@ -7,8 +7,11 @@ import enum
 # Déclarer la classe de base pour les modèles
 Base = declarative_base()
 
-
-
+class AnimeCategorie(enum.Enum):
+    vedette = 'vedette'
+    reguler = 'reguler'
+    recommend = 'recommended'
+   
 
 class Anime(Base):
    __tablename__ = 'anime'
@@ -16,8 +19,11 @@ class Anime(Base):
    title = Column(String, nullable=False)
    description = Column(String,nullable=False)
    genre = Column(String(50),nullable=False)
-   popularite =Column(Integer,default=0)
+   popularite = Column(Integer,default=0)
+   notation = Column(Integer,default=0)
+   categorie = Column(Enum(AnimeCategorie))
    date = Column(DateTime,default=func.now())
+   
   
 
 
